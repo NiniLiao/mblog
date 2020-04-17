db2d218a70e459a30c9983f34bc08f99aef39841
NiniLiao <u9513031@cmu.edu.tw>
2020-04-18 01:33:48 +0800

    def homepage

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,18 @@
+from django.template.loader import get_template
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
+def homepage (request):
+    templates = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = templates.render(locals())
+    # post_lists = list()
+    # for count, post in enumerate(posts):
+    #     post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
+    #     post_lists.append("<small>" + str(post.body.encode('utf-8'))+"</small><br><br>")
+    # return HttpResponse(post_lists)
+    return HttpResponse(html)
