9b274a1e612ae6eb139f4c2be3cff9552849b43e
NiniLiao <u9513031@cmu.edu.tw>
2020-04-18 01:35:36 +0800

    import url and add url of homepage

--- mblog/urls.py
+++ mblog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from mainsite.views import homepage
 
 urlpatterns = [
+    url(r'^$', homepage),
     path('admin/', admin.site.urls),
 ]
